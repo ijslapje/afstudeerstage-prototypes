4a11d7715ee9e9eb76da78c9e2de77ffaa30f1e8
Ian Robeerst <ianrobeerst@gmail.com>
2022-11-17 11:36:47 +0100

    Added Comments

--- Oefenen_Onderzoek/Api2Img2NFT_Demo/4-Diploma2NFT_v2/app.js
+++ Oefenen_Onderzoek/Api2Img2NFT_Demo/4-Diploma2NFT_v2/app.js
@@ -1,7 +1,13 @@
+//Import Librarys
+//$ npm install ipfs-core
+//$ npm install --save ethers
+//$ npm install --save jimp
+
 import * as IPFS from 'ipfs-core'
 import { createRequire } from 'module';
 import { stringify } from 'querystring';
 import { ethers } from "ethers";
+const Jimp = require("jimp");
 
 const require = createRequire(import.meta.url);
 const fs = require('fs');
@@ -13,18 +19,22 @@ let cursusTitle
 let cursusDescription
 let cursusStudytime
 let picturePath = './assets/diplomaIMG'
+
+//Leest image in dimploma path, zet in een for loop als er meer files zijn
 let file = fs.readdirSync(picturePath)
 let buffer = fs.readFileSync(`${picturePath}/${file}`)
-let fakePath = "https://imgur.com/a/Nl4QCxZ"
 
+//Gateway zetten voor ipfs
 let gateway = "https://ipfs.io/ipfs/"
+let imageGateway = "https://ipfs.cloudflare.com/ipfs/"
 
 
-//Leest de Json file op
+//Leest de Json file , dit is de "/v1/users/{id}/registrations" API call
 fs.readFile('./assets/apiData.json', (err, data) => {
     const jsonFile = JSON.parse(data);
     if (err) throw err;
 
+    //Kijkt voor diplomas waar user een toets voor heeft gedaan, die staan onder zijn eigen naam. Dat is alles behalve Hubper als naam
     for (let i = 0; i < jsonFile.length; i++) {
         if (jsonFile[i].registeredBy.firstname != "Hubper") {
             //Zet de data van de Json om naar de bijhoordende variable
@@ -36,24 +46,22 @@ fs.readFile('./assets/apiData.json', (err, data) => {
         }
     }
 
-
+    //Maakt de diploma met de gebruikers naam en cursus titel
     generateIMG(userFirstName, cursusTitle)
-    //Gebruik functie waar deze variables worden meegegeven
+
+    //Maakt de ipfs link voor de smart contract met daarin metadata.json
     createMetadata(userFirstName, cursusLanguage, cursusTitle, cursusDescription, cursusStudytime + " min")
 });
 
-//Importeer Jimp
-const Jimp = require("jimp");
-
+//Functie maak diploma image
 async function generateIMG(firstName, title) {
-
     //Leest de template image
     const image = await Jimp.read("./assets/handpickedDiploma.png");
 
     //Laad font in
     Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => {
         image
-            //Voeg tekst toe aan image
+            //Voeg tekst toe aan image, hardcoded x en y
             .print(
                 font,
                 190,
@@ -61,27 +69,36 @@ async function generateIMG(firstName, title) {
                 firstName,
                 50,
                 (err, image, { x, y }) => {
-                    image.print(font, x - 190, y + 30, title, 400);
+                    image.print(font, x - 190, y + 30, title, 400); //2de tekst die onder de eerste wordt toegevoegd
                 }
             )
-            //Slaat de image op tot een nieuwe image
+            //Slaat de image op tot een nieuwe image, voor meer diplomas zou het nummer van 1 steeds hoger worden
             .write('./assets/diplomaIMG/1.png');
     });
 };
 
-
+//Functie zet diploma img en metadata.json op een ipfs
 async function createMetadata(name, language, title, description, studytime) {
+    //Maakt lokaal een ipfs aan, dit zou dan straks een url zijn buiten lokaal om
     const ipfs = await IPFS.create({ repo: 'ok' + Math.random() })
-    //const buffer = fs.readFileSync(picturePath)
 
+    //Zet diploma img op de ipfs
     let { cid } = await ipfs.add(buffer)
 
+    //Zet de gegeven data + de ipfs van de diploma img in een json file
+    let metadata = JSON.stringify({ User_Name: name, Cursus_Language: language, Cursus_Title: title, Cursus_Description: description, Cursus_Studytime: studytime, IPFS: imageGateway + cid })
 
-    let metadata = JSON.stringify({ User_Name: name, Cursus_Language: language, Cursus_Title: title, Cursus_Description: description, Cursus_Studytime: studytime, IPFS: gateway + cid })
-
+    //Zet json file op ipfs
     cid = await ipfs.add(metadata)
+
+    //Zet link naar de ipfs om in een variable
+    let ipfsLink = gateway.concat(cid.path)
+   
+    //Console log cid info
     console.info(cid)
-    console.log(gateway.concat(cid.path))
+
+    //Console log link naar de IPFS
+    console.log(ipfsLink)
 }
 
 
