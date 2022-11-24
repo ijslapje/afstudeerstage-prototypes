0c8a5db3def958199099e405715b63f69b5707a3
Ian Robeerst <ianrobeerst@gmail.com>
2022-11-24 15:35:08 +0100

    jkbg

--- Smart Contracts/1_Learn2Earn/learn-to-earn/node_server/deployipfs.js
+++ Smart Contracts/1_Learn2Earn/learn-to-earn/node_server/deployipfs.js
@@ -1,36 +1,18 @@
-import { createMetadata } from "./app.js"
+import { createMetadata } from "./ipfs.js"
 import * as fs from "fs";
 import {spawn} from "node:child_process";
 
 const ipfs = await createMetadata()
 
 let link = JSON.stringify(ipfs)
+
 console.log(link)
 
 
 const contract = spawn('node', ['smart-contract/contract.js'], {
     env: {
         'IPFSLINK': link
-    },
-    networks: {
-        hardhat: {
-          chainId: 31337,
-          // allowUnlimitedContractSize: true,
-        },
-        localhost: {
-          chainId: 31337,
-          // allowUnlimitedContractSize: true,
-        },
-      },
-      solidity: {
-        version: "0.8.12",
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200
-          }
-        }
-      },
+    }
 })
 contract.stdout.on('data', (data) => console.log(`${data}`));
 contract.stderr.on('data', (data) => console.log(`${data}`) );
