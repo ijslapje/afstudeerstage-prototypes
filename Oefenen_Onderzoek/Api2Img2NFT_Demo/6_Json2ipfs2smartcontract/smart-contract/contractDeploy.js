893b16648ddc2e31776f5d44665ce8302f2f5b7b
IJslap <45030475+ijslapje@users.noreply.github.com>
2022-11-25 16:26:33 +0100

    ALLES WERKT

--- Oefenen_Onderzoek/Api2Img2NFT_Demo/6_Json2ipfs2smartcontract/smart-contract/contractDeploy.js
+++ Oefenen_Onderzoek/Api2Img2NFT_Demo/6_Json2ipfs2smartcontract/smart-contract/contractDeploy.js
@@ -17,22 +17,32 @@ const metadataTokens = JSON.parse(fs.readFileSync('./artifacts/contracts/trade.s
 
 
 async function main() {    
+
+    //Maak factory NFT
     const NFTfactory = new ethers.ContractFactory(metadataNFT.abi, metadataNFT.bytecode, wallet)
 
+    //Deploy smart contract met NFT naar de user en ipfs
     const NFTcontract = await NFTfactory.deploy(
         userPublicKey,
         ipfs
     )
 
+    //Maak factory Handpicked Tokens
     const Tokenfactory = new ethers.ContractFactory(metadataTokens.abi, metadataTokens.bytecode, wallet)
 
+    //Deploy smart contract met tokens naar de user en ipfs
     const TokenContract = await Tokenfactory.deploy(
         100000000, 
         50,
         userTokens
     )
 
-    console.log(await NFTcontract.baseUri(), await TokenContract.balanceOf(userPublicKey))
+    let bigNumber = await TokenContract.balanceOf(userPublicKey);
+    bigNumber = bigNumber/1000000000000000000;
+    
+
+    //Send NFT ipfs string en Aantal Handpicked Tokens
+    console.log(JSON.stringify({ baseUri: await NFTcontract.baseUri(), tokenContract: bigNumber.toString() }))
 }
 
 main().catch((error) => {
